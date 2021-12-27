9a40b69460fa9ea349bd050d98949dda35a6b297
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-12-27 20:08:39 +0200

    Try to add some build info html file
    Easier to see if deployment was completed

--- cloudflare_publish.sh
+++ cloudflare_publish.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+current_date=$(date --iso-8601=minutes)
+git_short_hash=$(git rev-parse --short HEAD)
+
+echo $current_date
+echo $git_short_hash
+
+sed -i "s/{0}/$current_date/g" src/wwwroot/index.html
+sed -i "s/{1}/$git_short_hash/g" src/wwwroot/index.html
+
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
